25e6a1d5e20fd4d6e1a1c60b6daeea40c1a716ef
sgarchavada <sgchavada2013@gmail.com>
2024-09-20 08:24:08 +0530

    add mvc for user

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseService } from './database/database.service';
 import { DatabaseModule } from './database/database.module';
 import { VillaModule } from './villa/villa.module';
 import { ReviewsModule } from './reviews/reviews.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, VillaModule, ReviewsModule],
+  imports: [DatabaseModule, VillaModule, ReviewsModule, UserModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
